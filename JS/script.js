010ab68c78097ff3dc9ac64eed739c02dd9bbf74
Mateusz Grincewicz <matek160393@gmail.com>
2021-11-25 20:24:11 +0100

    modification of functions

--- JS/script.js
+++ JS/script.js
@@ -1,19 +1,19 @@
 {
-    const resultCalculation = (sumElement, currencyElement) => {
+    const resultCalculate = (sum, currency,) => {
         const exchangeEUR = 0.22;
         const exchangeCZK = 5.57;
 
-        switch (currencyElement.value) {
+        switch (currency) {
             case "EUR":
-                return sumElement * exchangeEUR;
+                return sum * exchangeEUR;
             case "CZK":
-                return sumElement * exchangeCZK;
+                return sum * exchangeCZK;
         };
     };
-    const updateResultText = (sumElement, resultElement, currencyElement) => {
-        const resultElement = document.querySelector(".js-form__result");
-        resultElement.innertext = `${sumElement.tofixed(2)} PLN = ${resultElement.tofixed(2)} ${currencyElement} `;
 
+    const updateResultText = (result, sum, currency,) => {
+        const resultElement = document.querySelector(".js-form__result");
+        resultElement.innerText = `${sum} PLN = ${result} ${currency} `;
     };
     const onFormSubmit = (event) => {
         event.preventDefault();
@@ -21,17 +21,17 @@
         const sumElement = document.querySelector(".js-form__sum");
         const currencyElement = document.querySelector(".js-form__currency");
 
+
         const sum = +sumElement.value;
         const currency = currencyElement.value;
 
-        const result = resultCalculation(sum, currency);
+        const result = resultCalculate(sum, currency,);
 
-        updateResultText(sum, currency, result);
+        updateResultText(result, sum, currency,);
     };
     const init = () => {
         const formElement = document.querySelector(".js-form");
         formElement.addEventListener("submit", onFormSubmit);
-
     };
     init();
 };
